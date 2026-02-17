c7afb3b7ad5a58230d12781b40518e4fefae0d8d
Ean <72157891+whitestar1993@users.noreply.github.com>
2026-02-17 10:56:00 -0800

    vault backup: 2026-02-17 10:56:00

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,15 +8,15 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
-    pageTitleSuffix: "",
+    pageTitle: "The Archive",
+    pageTitleSuffix: "Timelines and guides",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "the-archive-1i2.pages.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
